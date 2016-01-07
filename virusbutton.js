3a5aefb3737e56dfe64e4c03dc7094df1c32487b
Winston Lu <mobcraft120@gmail.com>
2016-01-07 17:12:47 -0600

    Create virusbutton.js

---
+++ virusbutton.js
@@ -0,0 +1,22 @@
+function bob() {
+  var x = 0;
+  var rand;
+  var virusval;
+  if (x == 0) {
+    var lol = confirm("Your PC is infected! Download PCFix to fix it!");
+    if (lol == true) {
+      for (virusval = 0; virusval < 5; virusval++) {
+        rand = Math.floor((Math.random() * 3) + 1)
+        if (rand == 1) {
+          alert("SHOCKING TRUTH!  Microsoft Secret Revealed!");
+        }
+        if (rand == 2)
+          alert("PC Deals like you've never seen them! Click OK or else you miss out!");
+
+        if (rand == 3) {
+          alert("Your PC has the Trojan virus! Clean it up by getting Clean-Up!")
+        }
+      }
+    }
+  }
+}
